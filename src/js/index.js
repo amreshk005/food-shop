f318c7e9a0226fe928ca586dadb9134a09273246
amreshk005 <amreshk005@gmail.com>
2019-10-15 15:11:06 +0530

    Api list feched

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
-import str from "./models/Search";
-import {add as a, multiply as m, ID} from "./views/searchView";
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import {elements} from "./views/base";
 
-console.log(`Using imported function: ${a(ID, 2)} and ${m(3, 5)}, ${str}`);
+// Global state of the app
+// - Search object
+// - Current recipe object
+// - Shopping List object
+// - liked recipes
+
+const state = {};
+const controlSearch = async () => {
+  // 1) GET query from view
+  const query = searchView.getInput(); //1000
+  console.log(query);
+
+  if (query) {
+    //2) New Search object and add to state
+    state.search = new Search(query);
+
+    //3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    //4) Search for recipes
+    await state.search.getResults();
+
+    //5) render results on UI
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,23 @@
-export default "I am an export string.";
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults(query) {
+    //this is to access for data in any website
+    //   const proxy = "https://crossorigin.me/";
+    //   const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "c4b58f30fb4ae126cc3cce19cdeaa931";
+    try {
+      const res = await axios(
+        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      console.log(this.results);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
